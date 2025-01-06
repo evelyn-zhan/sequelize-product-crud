2313060765829ef86f88a5b3ac7659251740cf9c
Evelyn <evelynzanpx@gmail.com>
2025-01-06 10:04:59 +0700

    update: backend

--- index.js
+++ index.js
@@ -24,21 +24,74 @@ app.get('/create', (req, res) => {
     res.render('create')
 })
 
+app.get('/edit/:id', (req, res) => {
+    Product.findOne({ where: { id: req.params.id } })
+        .then((product) => {
+            res.render('edit', { product })
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).send('Internal Server Error')
+        })
+})
+
 app.post('/api/product', (req, res) => {
     Product.create({
         name: req.body.name,
         price: req.body.price
     })
         .then((result) => {
-            res.status(201).send({
+            res.status(201).json({
                 status: 201,
                 error: null,
                 response: result
             })
         })
         .catch((error) => {
-            res.status(502).send({
+            res.status(502).json({
                 status: 502,
+                error
+            })
+        })
+})
+
+app.put('/api/product/:id', (req, res) => {
+    Product.update(
+        {
+            name: req.body.name,
+            price: req.body.price
+        },
+        {
+            where: { id: req.params.id }
+        }
+    )
+        .then((result) => {
+            res.status(200).json({
+                status: 200,
+                error: null,
+                response: result
+            })
+        })
+        .catch((error) => {
+            res.status(502).json({
+                status: 502,
+                error
+            })
+        })
+})
+
+app.delete('/api/product/:id', (req, res) => {
+    Product.destroy({ where: { id: req.params.id } })
+        .then((result) => {
+            res.status(200).json({
+                status: 200,
+                error: null,
+                response: result
+            })
+        })
+        .catch((error) => {
+            res.status(500).json({
+                status: 500,
                 error
             })
         })
